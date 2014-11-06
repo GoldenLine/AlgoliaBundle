5a2e26931a7f6c7aa96cdc67e0e859241139d1c3
lapinski-pawel <pawel.lapinski@goldenline.pl>
2014-11-06 20:52:46 +0100

    Algolia client creation method

--- DependencyInjection/GoldenLineAlgoliaExtension.php
+++ DependencyInjection/GoldenLineAlgoliaExtension.php
@@ -19,5 +19,37 @@ class GoldenLineAlgoliaExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+
+        $this->createAlgoliaClient($container, $config);
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @param $config
+     * @throws \RuntimeException
+     * @throws \InvalidArgumentException
+     */
+    private function createAlgoliaClient(ContainerBuilder $container, $config)
+    {
+        if (!$container->hasDefinition('goldenline.algolia.client')) {
+            if (!class_exists('AlgoliaSearch\Client')) {
+                throw new \RuntimeException(
+                    'You must require "algolia/algoliasearch-client-php" to use the Algolia Bundle.'
+                );
+            }
+
+            // Validate the config
+            if (empty($config['credentials']['application_id']) || empty($config['credentials']['search_key'])) {
+                throw new \InvalidArgumentException(
+                    'The `application_id` and `search_key` must be set in your configuration file to use the Algolia Bundle'
+                );
+            }
+
+            $algoliaClient = new Definition('AlgoliaSearch\Client');
+            $algoliaClient->addArgument($config['credentials']['application_id']);
+            $algoliaClient->addArgument($config['credentials']['search_key']);
+
+            $container->setDefinition('goldenline.algolia.client', $algoliaClient);
+        }
     }
 }
