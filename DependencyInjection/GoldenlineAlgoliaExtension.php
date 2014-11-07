50c157cf7b8710877f0c38a7649bf5dd38741836
lapinski-pawel <pawel.lapinski@goldenline.pl>
2014-11-07 12:07:37 +0100

    Refactor creation Algolia Client

--- DependencyInjection/GoldenlineAlgoliaExtension.php
+++ DependencyInjection/GoldenlineAlgoliaExtension.php
@@ -16,7 +16,7 @@ class GoldenlineAlgoliaExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $config        = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
@@ -32,25 +32,16 @@ class GoldenlineAlgoliaExtension extends Extension
      */
     private function createAlgoliaClient(ContainerBuilder $container, $config)
     {
-        if (!$container->hasDefinition('goldenline.algolia.client')) {
-            if (!class_exists('AlgoliaSearch\Client')) {
-                throw new \RuntimeException(
-                    'You must require "algolia/algoliasearch-client-php" to use the Algolia Bundle.'
-                );
-            }
-
-            // Validate the config
-            if (empty($config['credentials']['application_id']) || empty($config['credentials']['search_key'])) {
-                throw new \InvalidArgumentException(
-                    'The `application_id` and `search_key` must be set in your configuration file to use the Algolia Bundle'
-                );
-            }
-
-            $algoliaClient = new Definition('AlgoliaSearch\Client');
-            $algoliaClient->addArgument($config['credentials']['application_id']);
-            $algoliaClient->addArgument($config['credentials']['search_key']);
-
-            $container->setDefinition('goldenline.algolia.client', $algoliaClient);
+        if (!class_exists('AlgoliaSearch\Client')) {
+            throw new \RuntimeException(
+                'You must require "algolia/algoliasearch-client-php" to use the Algolia Bundle.'
+            );
         }
+
+        $algoliaClient = new Definition('AlgoliaSearch\Client');
+        $algoliaClient->addArgument($config['credentials']['application_id']);
+        $algoliaClient->addArgument($config['credentials']['search_key']);
+
+        $container->setDefinition('goldenline.algolia.client', $algoliaClient);
     }
 }
